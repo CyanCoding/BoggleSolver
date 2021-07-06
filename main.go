1b9552808ee354bf61902784e72e303cc89eb89a
CyanCoding <camden.macleod@pm.me>
2021-07-06 18:20:30 -0400

    Create dicevalues array

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
 func main() {
+	var diceValues [16][6]string
+
+	diceValues[0][0] = "z"
+	diceValues[0][1] = "l"
+	diceValues[0][2] = "n"
+	diceValues[0][3] = "r"
+	diceValues[0][4] = "h"
+	diceValues[0][5] = "n"
+
+	diceValues[1][0] = "qu"
+	diceValues[1][1] = "n"
+	diceValues[1][2] = "m"
+	diceValues[1][3] = "i"
+	diceValues[1][4] = "h"
+	diceValues[1][5] = "v"
+
+	diceValues[2][0] = "d"
+	diceValues[2][1] = "s"
+	diceValues[2][2] = "y"
+	diceValues[2][3] = "i"
+	diceValues[2][4] = "t"
+	diceValues[2][5] = "t"
+
+	diceValues[3][0] = "w"
+	diceValues[3][1] = "a"
+	diceValues[3][2] = "o"
+	diceValues[3][3] = "t"
+	diceValues[3][4] = "o"
+	diceValues[3][5] = "t"
 
 }
