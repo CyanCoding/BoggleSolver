99a3b8c372bb688c9821e22c37f16051f99f9eff
CyanCoding <camden.macleod@pm.me>
2021-07-06 19:10:20 -0400

    Add greeting

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import "fmt"
+
 func main() {
-	diceValues := FillDice()
+	dice := RollDice()
+
+	fmt.Println("Welcome to the boggle solver/computer version!")
+	fmt.Println("Please choose one of the following")
+	fmt.Println("Solo mode (1), computer solve (2), input board (3)")
+	fmt.Print("Action > ")
+
+	var action int = 2
+	fmt.Scanln(&action)
+
+	if action < 1 || action > 3 { // Action is not valid
+		action = 2
+		fmt.Println("You did not enter a valid number. Defaulting to 2")
+	}
+
+	fmt.Println("Below is the board")
+	PrintDice(dice)
 }
