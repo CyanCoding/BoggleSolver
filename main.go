f45cb7f9d06deb0faf5ffa9ba1990c0aacb95f99
CyanCoding <camden.macleod@pm.me>
2021-07-06 19:12:38 -0400

    Fix blank lines and add if statement

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ func main() {
 
 	fmt.Println("Welcome to the boggle solver/computer version!")
 	fmt.Println("Please choose one of the following")
-	fmt.Println("Solo mode (1), computer solve (2), input board (3)")
+	fmt.Println("Solo mode (1), computer solve (2), input board (3)\n")
 	fmt.Print("Action > ")
 
 	var action int = 2
@@ -15,9 +15,11 @@ func main() {
 
 	if action < 1 || action > 3 { // Action is not valid
 		action = 2
-		fmt.Println("You did not enter a valid number. Defaulting to 2")
+		fmt.Println("\nYou did not enter a valid number. Defaulting to 2")
 	}
 
-	fmt.Println("Below is the board")
-	PrintDice(dice)
+	if action < 3 {
+		fmt.Println("Below is the board\n")
+		PrintDice(dice)
+	}
 }
