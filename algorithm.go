2a159b5257bcf4e05ece95ecd146d113be92d18d
CyanCoding <camden.macleod@pm.me>
2021-07-07 07:58:40 -0400

    Made test to see if word is correct

--- algorithm.go
+++ algorithm.go
@@ -1,5 +1,40 @@
 package main
 
+import "fmt"
+
+func TestWord(currentWord string) int {
+	for i := 0; i < len(words); i++ {
+		if len(words[i]) != len(currentWord) { // Word is not proper length
+			continue
+		}
+
+		if words[i] == currentWord {
+			return 0 // YES! Match
+		}
+
+		// Tests each letter to see if there are future possibilities
+
+		// BE CAREFUL, we use len(words[i]), which might be
+		// longer than len(currentWord) in some instances
+		// Make sure you break before you hit an error
+		for j := 0; j < len(words[i]); j++ {
+			// We've gotten past the length of the word without
+			// hitting an incorrect letter. This means there COULD
+			// be potential solutions, but we haven't found one yet
+			if len(words[i]) > len(currentWord) {
+				return 1 // Potential for future match
+			}
+
+			if words[i][j] != currentWord[j] { // Match failed, try next word
+				break
+			}
+		}
+	}
+
+	// We've gotten this far and haven't returned, so return bad
+	return 2 // FAIL, no match or future matches with available letters
+}
+
 // Recursively finds nearby board positions
 // In boggle, you can move vertically, horizontally, and diagonally
 func FindNearby(board [4][4]string, x int, y int, currentWord string) {
@@ -7,6 +42,15 @@ func FindNearby(board [4][4]string, x int, y int, currentWord string) {
 	// typical axis, y is in the first position because of the way the
 	// cookie crumbles. Don't mess with it!!
 	currentWord += board[y][x]
+
+	wordResult := TestWord(currentWord)
+	switch wordResult {
+	case 0: // The word is valid
+		fmt.Println(currentWord)
+		return
+	case 2: // Word and future words are invalid
+		return
+	}
 
 	// Can go west
 	if x > 0 {
